a1d72305299aadb7c18f42d7f67cf664b0f6b401
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-30 13:50:08 -0600

    Adds validation tests

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe InvoiceItem, type: :model do
     it { should belong_to :item }
     it { should belong_to :invoice }
   end
+
+  describe "Validations" do
+    it { should validate_presence_of :quantity }
+    it { should validate_numericality_of :quantity }
+    it { should validate_presence_of :unit_price }
+    it { should validate_numericality_of :unit_price }
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Transaction, type: :model do
   describe "Relationships" do
     it { should belong_to :invoice }
   end
+
+  describe "Validations" do
+    it { should validate_presence_of :result }
+  end
 end
