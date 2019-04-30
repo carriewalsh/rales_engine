cbccc92e38d1a4a501769a3dbd516a658c8b3ffb
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-30 13:41:27 -0600

    Adds relationships to models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
   describe "Relationships" do
+    it { should belong_to :customer }
+    it { should belong_to :merchant }
     it { should have_many :invoice_items }
     it { should have_many :items }
     it { should have_many :transactions }
