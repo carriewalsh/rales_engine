3967bf34419cdad3297f52f25dc1d4c700f6d7c4
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-04-30 15:48:10 -0600

    Converts into an api-only and adds index to controller

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,12 +1,14 @@
 require "rails_helper"
 
-RSpec.describe "Merchants API" do
+describe "Merchants API" do
   it "sends a list of merchants" do
     create_list(:merchant, 4)
 
     get "/api/v1/merchants"
-
     expect(response).to be_successful
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(4)
   end
 
   xit "can get one merchant by id" do
