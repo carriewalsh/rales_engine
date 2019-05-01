289d04c7ca0a72f272f2aad60ad14e5e48011807
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-01 15:54:48 -0600

    Adds date_revenue method for merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -11,4 +11,10 @@ class Merchant < ApplicationRecord
   def self.most_items(count)
     joins(invoices: :invoice_items).select("merchants.*, SUM(invoice_items.quantity) AS total").group(:id).order("total DESC").limit(count)
   end
+
+  def self.date_revenue(date)
+    beginning = date.to_datetime
+    end_of_day = beginning + 1.day - 1.minute
+    joins(invoices: :invoice_items).select("merchants.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue").where("invoice_items.updated_at BETWEEN ? AND ?", beginning, end_of_day).group(:id).order("revenue DESC")
+  end
 end
