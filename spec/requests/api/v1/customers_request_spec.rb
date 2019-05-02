0584c531c34f97d7c1b8be07002909b35fd5ceee
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-02 08:02:21 -0600

    Adds customer show request

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -6,11 +6,21 @@ RSpec.describe "Customers API" do
     @cust49 = Customer.create(first_name: "Reynold", last_name: "Beed")
     @cust50 = Customer.create(first_name: "Christiano", last_name: "Trighton")
   end
+
   it "sends a list of customers" do
     get "/api/v1/customers"
     expect(response).to be_successful
 
     customers = JSON.parse(response.body)
     expect(customers["data"].count).to eq(3)
+  end
+
+  it "can get a single customer by id" do
+    id = @cust48.id
+    get "/api/v1/customers/#{id}"
+    expect(response).to be_successful
+
+    customer = JSON.parse(response.body)["data"]
+    expect(customer["attributes"]["id"].to_i).to eq(id)
   end
 end
