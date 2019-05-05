77be4cebac2c273d19952863b4d8a64663a95d79
Carrie Walsh <43975624+carriewalsh@users.noreply.github.com>
2019-05-04 19:06:53 -0600

    Adds attributes to invoice item serializer

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,6 +1,6 @@
 class InvoiceItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :quantity, :unit_price
+  attributes :id, :quantity, :unit_price, :item_id, :invoice_id
 
   belongs_to :invoice
   belongs_to :item
